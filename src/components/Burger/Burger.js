e98fbcf2d7d57a3ef7d355e7ae841edd54a82b68
See Yang <seeyang24@gmail.com>
2019-10-31 19:48:19 -0500

    added mapping of ingredients to burger.js

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,22 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import './Burger.css';
 
 const burger = (props) => {
+    // gives you an array of the keys from an object
+    // igKey = ingredient key
+    // spread operator creates a new array -- access the given key of the ingredient
+    const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) =>{
+            console.log(igKey)
+            return <BurgerIngredient key={igKey + i} type={igKey} /> // igKey is like salad, and i is like 1,2,3.
+        });
+    });
+
+    console.log(props.ingredients);
+
     return (
         <div className='Burger'>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
